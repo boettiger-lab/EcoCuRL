51c7ba1dd8b694973a50a7fbb55e72a46a6c28d9
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-10-10 10:57:10 -0700

    remote fn syntax

--- src/esc_script.py
+++ src/esc_script.py
@@ -1,11 +1,12 @@
 import pandas as pd
+import ray
 
 from escapement import escapement_policy
 from envs.custom_example import fishing_env
 from envs.custom_example import curriculum_fishing_env # only for the curriculum
 
-esc = escapement_policy(n_sp=1, n_act=1, controlled_sp=[0], max_esc=1)
-esc_results = {i: esc.rand_policy_search(env=fishing_env(*lvl)) for i, lvl in CURRICULUM.items()}
+esc = escapement_policy.remote(n_sp=1, n_act=1, controlled_sp=[0], max_esc=1)
+esc_results = {i: ray.get(esc.rand_policy_search(env=fishing_env(*lvl))) for i, lvl in CURRICULUM.items()}
 
 print("results:\n"
       "--------\n"
