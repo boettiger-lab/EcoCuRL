401ff5996c535046eae5d4ab57c5483cba742ef4
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-10-10 12:18:34 -0700

    debug

--- src/esc_script.py
+++ src/esc_script.py
@@ -9,21 +9,21 @@ from envs.custom_example import curriculum_fishing_env # only for the curriculum
 CURRICULUM = curriculum_fishing_env(config={"start_level":0}).CURRICULUM
 esc = escapement_policy.remote(n_sp=1, n_act=1, controlled_sp=[0], max_esc=1)
 
+@ray.remote(num_gpus=2)
 def sample_esc_benchmark(lvl, esc, samples=1000):
     env = fishing_env(**CURRICULUM[lvl])
     policies = ray.get([esc.sample_policy.remote() for _ in range(1000)])
-    return ray.get([
+    return [
         (
             str(esc_vec),
-            [
+            ray.get([
                 esc.sample_policy_reward.remote(esc_vec, env) for _ in range(50)
-            ] 
+            ])
         )
         for esc_vec in policies
-    ])
+    ]
 
 def get_stats(benchmarks):
-    """ benchmarks = output of sample_esc_benchmark """
     return (
         {esc: np.mean(results) for esc, results in benchmarks}, 
         {esc: np.std(results) for esc, results in benchmarks},
@@ -32,9 +32,9 @@ def get_stats(benchmarks):
 
 esc_results = {i: 
     get_stats(
-        sample_esc_benchmark(
+        ray.get(sample_esc_benchmark.remote(
             lvl=i, esc=esc)
-        )[0]
+        ))[0]
     for i in range(4)
 }
 
