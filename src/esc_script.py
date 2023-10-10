0b31087a051bacedf2b2fff47ba0ea2f48d06010
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-10-10 12:24:41 -0700

    making remote work

--- src/esc_script.py
+++ src/esc_script.py
@@ -7,7 +7,7 @@ from envs.custom_example import fishing_env
 from envs.custom_example import curriculum_fishing_env # only for the curriculum
 
 CURRICULUM = curriculum_fishing_env(config={"start_level":0}).CURRICULUM
-esc = escapement_policy.remote(n_sp=1, n_act=1, controlled_sp=[0], max_esc=1)
+esc = escapement_policy(n_sp=1, n_act=1, controlled_sp=[0], max_esc=1)
 
 @ray.remote
 def sample_esc_benchmark(lvl, esc, samples=1000):
