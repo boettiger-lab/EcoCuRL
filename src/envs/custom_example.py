6553b7971513eefe5fb32d7e3338810fcb61ea81
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-10-10 13:09:11 -0700

    debug

--- src/envs/custom_example.py
+++ src/envs/custom_example.py
@@ -58,7 +58,7 @@ class fishing_env(gym.Env):
 			)
 
 		# dynamics
-		print(f"pop: {pop}, harvest: {harvest}")
+		print(f"action: {action}, quota: {quota}, pop: {pop}, harvest: {harvest}")
 		pop -= harvest
 		pop += (
 			self.r * pop * (1 - pop / self.K) 
