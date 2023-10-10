5ecd704a6fd48d0d8ffc162d69e1ddfb8b43e261
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-10-10 10:59:03 -0700

    mini

--- src/esc_script.py
+++ src/esc_script.py
@@ -5,7 +5,7 @@ from escapement import escapement_policy
 from envs.custom_example import fishing_env
 from envs.custom_example import curriculum_fishing_env # only for the curriculum
 
-CURRICULUM = curriculum_fishing_env().CURRICULUM
+CURRICULUM = curriculum_fishing_env(config={"start_level":0}).CURRICULUM
 esc = escapement_policy.remote(n_sp=1, n_act=1, controlled_sp=[0], max_esc=1)
 esc_results = {i: ray.get(esc.rand_policy_search(env=fishing_env(*lvl))) for i, lvl in CURRICULUM.items()}
 
