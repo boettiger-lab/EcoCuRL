3b8e1acbaaa6da9d93d8c3f53a6e685c0843bdf9
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-10-09 18:04:11 -0700

    bugs

--- src/envs/custom_example.py
+++ src/envs/custom_example.py
@@ -119,7 +119,7 @@ class curriculum_fishing_env(TaskSettableEnv):
 		# Level 3: x100, etc..
 		# also normalize by ep_len so 'naked' episode rewards are in [0, 100]
 		reward = (10 ** (self.cur_level)) * (rew / self.env.ep_len) * 100
-		print(f"lvl: {self.cur_level}, reward: {reward}")
+		print(f"lvl: {self.cur_level}, reward: {reward}, step: {self.env.timestep}")
 		return (
 			obs, 
 			reward, 
