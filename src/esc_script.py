f433dc3d56fbed543609a4ab77400ad8c27131a4
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-10-11 12:27:45 -0700

    finished new compute logic

--- src/esc_script.py
+++ src/esc_script.py
@@ -10,40 +10,46 @@ CURRICULUM = curriculum_fishing_env(config={"start_level":0}).CURRICULUM
 esc = escapement_policy(n_sp=1, n_act=1, controlled_sp=[0], max_esc=1)
 
 @ray.remote(num_cpus=20)
-def sample_esc_benchmark(lvl, esc, samples=1000):
+def benchmark(esc_vec, esc_obj, env):
+    results = [esc_obj.sample_policy_reward(esc_vec, env) for _ in range(50)]
+    print(f"escapement = {esc_vec} done!")
+    return np.mean(results)
+        
+def sample_esc_benchmark(lvl, esc_obj, samples=1000):
     env = fishing_env(**CURRICULUM[lvl])
-    policies = [esc.sample_policy() for _ in range(1000)]
-    return [
-        (
-            str(esc_vec),
-            [
-                esc.sample_policy_reward(esc_vec, env) for _ in range(50)
-            ]
-        )
-        for esc_vec in policies
-    ]
+    policies = [esc_obj.sample_policy() for _ in range(1000)]
+    return policies, np.array(ray.get([benchmark(esc_vec, esc_obj, env) for esc_vec in policies]))
 
-def get_stats(benchmarks):
-    return (
-        {esc: np.mean(results) for esc, results in benchmarks}, 
-        {esc: np.std(results) for esc, results in benchmarks},
-        )
+# def get_stats(policies, benchmarks):
+
+#     return (
+#         {esc: np.mean(results) for esc, results in benchmarks}, 
+#         {esc: np.std(results) for esc, results in benchmarks},
+#         )
+
+def find_best_policy(policies, esc_results):
+    optimal_idx = np.argmax(esc_results)
+    return policies[optimal_idx], esc_results[optimal_idx]
+
+lvl_escapement_benchmarks = {}
+for lvl in range(4):
+    policies, esc_results = sample_esc_benchmark(lvl, esc_obj, samples=1000)
+    esc, rew = find_best_policy(policies, esc_results)
+    lvl_escapement_benchmarks[lvl] = {'esc': esc, 'rew': rew}
+    print({'esc': esc, 'rew': rew})
 
 
-esc_results = {i: 
-    get_stats(
-        ray.get(sample_esc_benchmark.remote(
-            lvl=i, esc=esc)
-        ))[0]
-    for i in range(4)
-}
+# esc_results = {i: 
+#     ray.get(sample_esc_benchmark(lvl=i, esc=esc))
+#     for i in range(4)
+# }
 
-print("results:\n"
-      "--------\n"
-      "{"
-    )
-for i, result in esc_results:
-    print(
-        f"{i}: {result}"
-        )
-print("}")
+# print("results:\n"
+#       "--------\n"
+#       "{"
+#     )
+# for i, result in esc_results:
+#     print(
+#         f"{i}: {result}"
+#         )
+# print("}")
