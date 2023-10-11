7c3669b9c3372cf904dca8b0ff327fccb28141b7
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-10-11 15:47:32 -0700

    mini

--- src/util.py
+++ src/util.py
@@ -7,7 +7,7 @@ def simulate(env, policy):
 		s = env.state
 		p = env.state_to_pop(s)
 		a = policy(p)
-		action = [env.effort_to_action(a)]
+		action = env.effort_to_action(a)
 		obs, rew, terminated, done, info = env.step(action)
 		timeseries.append(s)
 		actionseries.append(a)
