e9e289dce68062b32c88830b6568e2cfabc2b6a5
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-10-17 11:17:56 -0700

    debug

--- src/chatgpt_queries/train.py
+++ src/chatgpt_queries/train.py
@@ -334,7 +334,7 @@ def set_task_callback(info):
     from util import dict_pretty_print
     print("\n"*5)
     dict_pretty_print(info)
-    print("\n"*5)
+    print("\n"*5 + "info:\n\n")
     obs = info["obs"][info["agent"]]
     agent_1_policy = info["policy_map"]["agent_1"]
     agent_2_task = agent_1_policy.compute_actions([obs])[0]  # Use agent 1's policy to determine the task
