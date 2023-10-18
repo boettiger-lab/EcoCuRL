8e88067792f53d54c30a770c00b50202d9bed159
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-10-18 15:45:20 -0700

    debug

--- src/chatgpt_queries/train.py
+++ src/chatgpt_queries/train.py
@@ -104,19 +104,6 @@ class MyEnv(MultiAgentEnv, TaskSettableEnv):
         rew2 = harvest - cost - penalty
         rew1 = -rew2 * self.r # devalue the easy strategy of just choosing low r values
 
-        print(f"""
-step summary:
-
-timestep = {self.timestep}
-action_dict = {action_dict}
-pop  = {self.pop_beginning}
-pop' = {self.pop}
-harv = {harvest}
-cost = {cost}
-rewards = 1: {rew1}, 2: {rew2}
-            """
-            )
-
         self.timestep += 1
         done = {
             self.agent1: self.timestep >= self.max_steps, 
@@ -132,6 +119,20 @@ rewards = 1: {rew1}, 2: {rew2}
         obs_dict = {
             self.agent2: self.pop
         }
+
+                print(f"""
+step summary:
+
+timestep = {self.timestep}
+action_dict = {action_dict}
+pop  = {self.pop_beginning}
+pop' = {self.pop}
+harv = {harvest}
+cost = {cost}
+rewards = 1: {rew1}, 2: {rew2}
+obs = {obs_dict}
+            """
+            )
 
         return obs_dict, rew_dict, done, {'__all__': False}, {}
 
