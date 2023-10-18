6fcef3109f6be38cfca1056c339d7fcb3048ca21
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-10-17 17:13:13 -0700

    debug

--- src/chatgpt_queries/train.py
+++ src/chatgpt_queries/train.py
@@ -384,7 +384,7 @@ class CustomCallbacks(DefaultCallbacks):
         **kwargs
     ):
         print(type(episode))
-        print(episode)
+        dict_pretty_print(episode.__dict__)
         # Check if there are multiple episodes in a batch, i.e.
         # "batch_mode": "truncate_episodes".
         # if worker.config.batch_mode == "truncate_episodes":
