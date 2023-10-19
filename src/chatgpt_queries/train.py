b4466ac1404c304f66e98be15ad4c0162f7f2092
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-10-19 12:47:47 -0700

    debug

--- src/chatgpt_queries/train.py
+++ src/chatgpt_queries/train.py
@@ -549,7 +549,7 @@ class CustomCallbacks(DefaultCallbacks):
         # # obs -> confirmed: this is equal to obs
         # obs_val_ = result['custom_metrics']['avg_rew2_mean']
         # obs_ = np.float32([obs_val])
-        r_obs = result['custom_metrics']['r_val']
+        # r_obs = result['custom_metrics']['r_val']
 
         # agent 1 tracking
         agent_1_rew = result['custom_metrics']['avg_rew1_mean']
@@ -558,6 +558,7 @@ class CustomCallbacks(DefaultCallbacks):
             print("\n"*2)
             print("On train result")
             # dict_pretty_print(result)
+            print("custom_metrics: ", result['custom_metrics'])
             print('r observation: ', r_obs)
             print("rew2 on train batch:  ", obs)
             print("agent 1 action: ", agent_1_action[0])
