1c149cf012fd10ae4339e862e6bec822188460ba
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-10-23 15:55:48 -0700

    imports on __init__.py actualized

--- ecocurl/envs/__init__.py
+++ ecocurl/envs/__init__.py
@@ -1,2 +1,2 @@
-from ecocurl.envs.benchmarked_curl_env import curlMultitasker
+from ecocurl.envs.benchmarked_curl_env import benchmarkedEnv, discrBenchMultitasker
 from ecocurl.envs.fishing import fishing_1s1a
