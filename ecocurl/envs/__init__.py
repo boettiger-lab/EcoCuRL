548a6f0618c7498bc741cb50ec9b9fafca8c2586
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-10-25 15:10:23 -0700

    mini

--- ecocurl/envs/__init__.py
+++ ecocurl/envs/__init__.py
@@ -1,2 +1,2 @@
-from ecocurl.envs.benchmarked_curl_env import benchmarkedEnv, discrBenchMultitasker
+from ecocurl.envs.benchmarked_curl_env import benchmarkedEnv, discrBenchMultitasker, discrBenchMultitaskerV2
 from ecocurl.envs.fishing import fishing_1s1a
