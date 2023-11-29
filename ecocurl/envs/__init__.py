961ef89ae3176931821ced0b4ead2e2b530868c2
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-11-29 10:46:32 -0800

    minibug

--- ecocurl/envs/__init__.py
+++ ecocurl/envs/__init__.py
@@ -1,4 +1,4 @@
 from ecocurl.envs.benchmarked_curl_env import benchmarkedEnv, discrBenchMultitasker, discrBenchMultitaskerV2
 from ecocurl.envs.fishing import fishing_1s1a
 from ecocurl.envs.adv_env import AdversarialExampleEnv
-from ecocurl.envs.adv_callbacks import AdversarialCallbacks
+from ecocurl.envs.adv_callbacks import get_AdvCallbck
