c38caf25314f5352a34fdf4626ecc718ee8f72b0
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-11-30 14:08:05 -0800

    typo

--- examples/plot_policy.py
+++ examples/plot_policy.py
@@ -15,8 +15,7 @@ if __name__ == "__main__":
 		"ray_results",
 		"PPO_2023-11-30_20-28-13",
 		"PPO_discrBenchMultitaskerV2_fc438_00000_0_2023-11-30_20-28-13",
-		"checkpoint_000000"
-		"checkpoint_000000"
+		"checkpoint_000000",
 	)
 	plot_target = os.path.join(
 		"..",
